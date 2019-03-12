869bb9d667dd26e0357e596ab942258ffa41bf0c
Stasiek <42705773+StasiekESX@users.noreply.github.com>
2019-03-13 00:36:32 +0100

    Add files via upload
    Skrypciki są na serwerze trzeba je przetestować, jak się nauczę to zrobi się to pod telefony itd.

--- Stasiek/Stasiek.cs
+++ Stasiek/Stasiek.cs
@@ -17,20 +17,24 @@ namespace Stasiek
 
         private async Task ButtonCheck()
         {
+            //[X] => Usuwa wszystkie Taski z Peda czyli anuluje animacje itd.
             if (IsControlJustReleased(0, 73))
             {
                 ClearPedTasks(PlayerPedId());
             }
 
+            //[F2] => Animacja podnoszenia rąk.
             if (IsControlJustReleased(0, 289))
             {
                 PlayAnimationOnPlayerPed("missminuteman_1ig_2", "handsup_enter", 50);
             }
+            //[G] => Animacja założenia rąk.
             else if (IsControlJustReleased(0, 47))
             {
                 PlayAnimationOnPlayerPed("amb@world_human_hang_out_street@female_arms_crossed@enter", "enter", 50);
             }
 
+            //[B] => Animacja pokazywanie palcem.
             if (IsControlJustReleased(0, 29))
             {
                 RequestAnimDict("anim@mp_point");
@@ -44,14 +48,17 @@ namespace Stasiek
             await Task.FromResult(0);
         }
 
+        //Funkcja uruchamiająca animację
         private async void PlayAnimationOnPlayerPed(string lib, string anim, int flag)
         {
+            //Jeżelu nie gra tej animacji to niech zacznie
             if (!IsEntityPlayingAnim(PlayerPedId(), lib, anim, 3))
             {
                 RequestAnimDict(lib);
                 while (!HasAnimDictLoaded(lib)) await Delay(1);
                 TaskPlayAnim(PlayerPedId(), lib, anim, 1.0f, 1.0f, -1, flag, 0f, false, false, false);
             }
+            //W przeciwnym wypadku jeżeli już gra tę animację to czyścimy Ped'owi aktualne Taski
             else
             {
                 ClearPedTasks(PlayerPedId());
--- Stasiek/VoiceManager.cs
+++ Stasiek/VoiceManager.cs
@@ -10,40 +10,50 @@ namespace Stasiek
 {
     public class VoiceManager : BaseScript
     {
+        /// Tablica gdzie można dodawać dowolną liczbę ustawień promixy dźwięku
         float[] voice = new float[] { 2.0f, 12.0f, 40.0f };
+        string[] voiceLabel = new string[] { "Szept", "Normalny", "Krzyk"};
         int v = 0;
         bool debug = true;
+
         public VoiceManager()
         {
+            Tick += ChangeVoicePromixy;
             Tick += ChangeVoicePromixy;
         }
 
         private async Task ChangeVoicePromixy()
         {
+            //Zmiana dźwięku F5
             if (IsControlJustReleased(0, 166))
             {
-                if (v == 2)
+                if (v == voice.Length - 1)
                 {
                     v = -1;
                 }
                 v++;
                 NetworkSetTalkerProximity(voice[v]);
+                Debug.Write("Stasiek.VoiceManager => Zmieniono zasieg Voice'a na " + voiceLabel[v]);
             }
+            //Debuger pokazuje Sphere który oznajmia zasięg dźwięku
             if (debug)
             {
                 Vector3 coords = GetEntityCoords(PlayerPedId(), true);
                 DisplayDebug(coords.X, coords.Y, coords.Z, voice[v]);
             }
+            //Komenda na wyłączenie debuggera
             RegisterCommand("voicemanager_debug", new Action<int, List<object>, string>(async (source, args, raw) =>{ debug = !debug; }), false);
 
             await Task.FromResult(0);
         }
 
+        //Ustawiania promixy(zasięgu) voica
         private void SetDefaultPromixy()
         {
             NetworkSetTalkerProximity(voice[v]);
         }
 
+        //Wyświetlanie Sphere dla debugera
         private void DisplayDebug(float x, float y, float z, float s)
         {
             DrawMarker(28, x, y, z, 0.0f, 0.0f, 0.0f, 0, 0.0f, 0.0f, s, s, s, 0, 255, 0, 30 * (v + 1), false, true, 2, false, null, null, false);
