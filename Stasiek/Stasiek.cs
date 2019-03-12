227c34b2af07949fe7f49df2a13ca9266c13b46f
Stasiek <42705773+StasiekESX@users.noreply.github.com>
2019-03-13 00:10:19 +0100

    Nowe skrypciki grzebanko masne było

---
+++ Stasiek/Stasiek.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using static CitizenFX.Core.Native.API;
+
+
+namespace Stasiek
+{
+    public class Stasiek : BaseScript
+    {
+        public Stasiek()
+        {
+            Tick += ButtonCheck;
+        }
+
+        private async Task ButtonCheck()
+        {
+            if (IsControlJustReleased(0, 73))
+            {
+                ClearPedTasks(PlayerPedId());
+            }
+
+            if (IsControlJustReleased(0, 289))
+            {
+                PlayAnimationOnPlayerPed("missminuteman_1ig_2", "handsup_enter", 50);
+            }
+            else if (IsControlJustReleased(0, 47))
+            {
+                PlayAnimationOnPlayerPed("amb@world_human_hang_out_street@female_arms_crossed@enter", "enter", 50);
+            }
+
+            if (IsControlJustReleased(0, 29))
+            {
+                RequestAnimDict("anim@mp_point");
+                while (!HasAnimDictLoaded("anim@mp_point")) await Delay(1);
+                SetPedCurrentWeaponVisible(PlayerPedId(), false, true, true, true);
+                SetPedConfigFlag(PlayerPedId(), 36, true);
+                TaskMoveNetwork(PlayerPedId(), "task_mp_pointing", 0.5f, false, "anim@mp_point", 24);
+                RemoveAnimDict("anim@mp_point");
+            }
+
+            await Task.FromResult(0);
+        }
+
+        private async void PlayAnimationOnPlayerPed(string lib, string anim, int flag)
+        {
+            if (!IsEntityPlayingAnim(PlayerPedId(), lib, anim, 3))
+            {
+                RequestAnimDict(lib);
+                while (!HasAnimDictLoaded(lib)) await Delay(1);
+                TaskPlayAnim(PlayerPedId(), lib, anim, 1.0f, 1.0f, -1, flag, 0f, false, false, false);
+            }
+            else
+            {
+                ClearPedTasks(PlayerPedId());
+            }
+        }
+    }
+    
+}
---
+++ Stasiek/VoiceManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using static CitizenFX.Core.Native.API;
+
+
+namespace Stasiek
+{
+    public class VoiceManager : BaseScript
+    {
+        float[] voice = new float[] { 2.0f, 12.0f, 40.0f };
+        int v = 0;
+        bool debug = true;
+        public VoiceManager()
+        {
+            Tick += ChangeVoicePromixy;
+        }
+
+        private async Task ChangeVoicePromixy()
+        {
+            if (IsControlJustReleased(0, 166))
+            {
+                if (v == 2)
+                {
+                    v = -1;
+                }
+                v++;
+                NetworkSetTalkerProximity(voice[v]);
+            }
+            if (debug)
+            {
+                Vector3 coords = GetEntityCoords(PlayerPedId(), true);
+                DisplayDebug(coords.X, coords.Y, coords.Z, voice[v]);
+            }
+            RegisterCommand("voicemanager_debug", new Action<int, List<object>, string>(async (source, args, raw) =>{ debug = !debug; }), false);
+
+            await Task.FromResult(0);
+        }
+
+        private void SetDefaultPromixy()
+        {
+            NetworkSetTalkerProximity(voice[v]);
+        }
+
+        private void DisplayDebug(float x, float y, float z, float s)
+        {
+            DrawMarker(28, x, y, z, 0.0f, 0.0f, 0.0f, 0, 0.0f, 0.0f, s, s, s, 0, 255, 0, 30 * (v + 1), false, true, 2, false, null, null, false);
+        }
+    }
+
+}
