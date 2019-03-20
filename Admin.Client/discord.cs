388e2124c9fdbad2da99fd6fc532cd0f8ddc9251
Paweł Łukasiewicz <pawelpllukasiewicz@gmail.com>
2019-03-20 01:41:37 +0100

    Poprawienie kilku błędów ort // TODO dodać aby działało na OneSyncu

--- Admin.Client/discord.cs
+++ Admin.Client/discord.cs
@@ -18,7 +18,8 @@ namespace Admin.Client
             // Pobieranie ID Gracza
             var id = Game.Player.ServerId;
             // Pobieranie Wszystkich Graczy na serwerze
-            var players = GetNumberOfPlayers();
+            //var players = GetNumberOfPlayers();
+            var players = NetworkGetNumConnectedPlayers();
             // Pobieranie Nicku gracza (Później będzie zmienione na imię i nazwisko ic postaci)
             var playername = Game.Player.Name;
             // Tytuł, w co dany gracz gra.
