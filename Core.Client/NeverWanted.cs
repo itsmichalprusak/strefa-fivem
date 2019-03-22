6f700dbe4843452638a9180cca9d87bcaf13c143
Paweł Łukasiewicz <pawelpllukasiewicz@gmail.com>
2019-03-22 16:02:28 +0100

    Dodanie klasy NeverWanted - odpowiedzialna za poziom scigania przez policje ktora jest w GTA5

---
+++ Core.Client/NeverWanted.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+namespace Core.Client
+{
+    public class NeverWanted : BaseScript
+    {
+         
+         public NeverWanted()
+         {
+             Tick += timer;
+             EventHandlers["onClientResourceStart"] += new Action<string>(OnClientResourceStart);
+         }
+         
+         private void OnClientResourceStart(string resourceName)
+         {
+             if (GetCurrentResourceName() != resourceName) return;
+         }
+
+         private async Task timer()
+         {
+             // Jesli Wanted Jest wiekszy badz rowna sie 0 to
+             if (GetPlayerWantedLevel(PlayerId()) >= 0)
+             {
+                 //Ustawia Wanted na 0
+                 SetPlayerWantedLevel(PlayerId(), 0, false);
+                 SetPlayerWantedLevelNow(PlayerId(), false);
+             }
+             await Delay(1000);
+         }
+    }
+}
