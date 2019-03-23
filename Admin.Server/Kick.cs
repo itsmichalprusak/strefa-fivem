31ad67495dab81f2348a5775c7e366c8348f47a0
Paweł Łukasiewicz <sas123.1999@gmail.com>
2019-03-23 17:16:30 +0100

    Reupload

---
+++ Admin.Server/Kick.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+
+namespace Admin.Server
+{
+    class Kick : BaseScript
+    {
+        public Kick()
+        {
+            EventHandlers["srp_admin:kickReson"] += new Action<Player,int,string>(OnPlayerKickReson);
+            EventHandlers["srp_admin:kick"] += new Action<Player, int>(OnPlayerKick);
+        }
+
+        // Defaultowy powod kicka
+        private void OnPlayerKick([FromSource] Player source, int id)
+        {
+            OnPlayerKickReson(source, id, "You've been kicked");
+        }
+
+        // Kick sam w sobie
+        private void OnPlayerKickReson([FromSource] Player source, int id, string reason)
+        {
+            // Debug w konsoli serwera
+            Debug.WriteLine($"[DeBug] SerwerEwent Kick dla id: '{id}' => '{reason}'");
+            // Funckja odpowiedzialna za kicki
+            API.DropPlayer(id.ToString(), $"Kick: {reason}");
+        }
+    }
+}
